872067bf7c5d0143894a871956b7f1224814b4f8
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-26 15:09:11 -0300

    Implement messsages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def create
+    @chatroom = Chatroom.find params[:chatroom_id]
+    @user = current_user
+    @message = Message.new(strong_params)
+    @message.user = @user
+    @message.chatroom = @chatroom
+
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render 'chatrooms/show'
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:message).permit(:content)
   end
 end
